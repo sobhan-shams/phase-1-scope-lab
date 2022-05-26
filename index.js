4e15f57cc6163e7a876b8cf534d6a786df4ef498
sobhan <somayahshams1995@gmail.com>
2022-05-26 15:45:40 +0300

    solve assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+    return customerName;
+}
+
+function setBestCustomer()
+{ bestCustomer = "not bob";
+ return bestCustomer;
+}
+//console.log(bestCustomer);
+function overwriteBestCustomer(){
+bestCustomer = "maybe bob";
+return bestCustomer;
+}
+function changeLeastFavoriteCustomer(){
+    const leastFavoriteCustomer = "abc";
+
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "abc";
+    throw 'Assignment to constant variable.'
+
+}
